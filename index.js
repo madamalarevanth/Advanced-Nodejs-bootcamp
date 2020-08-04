04fe1ca879de21fcc01fbc8f0dc547fd263524f2
madamalarevanth <revanthrex1001@gmail.com>
2020-08-04 18:53:32 +0530

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express();
 
+function doWork(duration){
+    const start = Date.now();
+    while(Date.now()-start<duration){}
+}
+
 app.get('/',(req,res)=>{
+    doWork(5000);
     res.send('Hi there');
     
 })
