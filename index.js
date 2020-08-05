54811f19d51cfd6a08f96f6ff3c9675ee8950f97
madamalarevanth <revanthrex1001@gmail.com>
2020-08-05 14:51:24 +0530

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const cluster = require('cluster')
 
 if(cluster.isMaster){
-    cluster.fork()
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+
 }else{
     const express = require('express')
 const app = express();
@@ -15,6 +18,10 @@ app.get('/',(req,res)=>{
     doWork(3000);
     res.send('Hi there');
     
+})
+
+app.get('/fast',(req,res)=>{
+    res.send('This is fast')
 })
 app.listen(3000);
 }
