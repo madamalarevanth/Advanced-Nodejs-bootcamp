e34938fcf1b0150d125d67a55d2924ef3c8fcd9f
madamalarevanth <revanthrex1001@gmail.com>
2020-08-05 15:21:40 +0530

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster')
 
-if (cluster.isMaster) {
-    cluster.fork();
-    cluster.fork();
+const express = require('express')
+const app = express();
 
-} else {
-    const express = require('express')
-    const app = express();
-
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) { }
-    }
-
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            console.log('2:', Date.now() - start);
-        });
-        //doWork(3000);
-        res.send('Hi there');
-
-    })
-
-    app.get('/fast', (req, res) => {
-        res.send('This is fast')
-    })
-    app.listen(3000);
+function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) { }
 }
+
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        console.log('2:', Date.now() - start);
+    });
+    //doWork(3000);
+    res.send('Hi there');
+
+})
+
+app.get('/fast', (req, res) => {
+    res.send('This is fast')
+})
+app.listen(3000);
